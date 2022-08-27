f8a295e136ea7b84035994904575ac8c024b8ad7
rachid j <rachidj@rachids-Mac-mini.local>
2022-08-27 19:55:20 +0200

    Mise en place du Content

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,3 +1,19 @@
+import styles from "./Content.module.scss";
+
 export default function Content() {
-    return <div className="flex-fill b2">Content</div>
+    return (
+        <div className="flex-fill container pt-30">
+            <h1 className="my-30">Decouvrez nos nouvelles recettes</h1>
+            <div className={`card p-20 ${styles.contentCard}`}>
+                <div className={styles.grid}>
+                    <div className={styles.elem}>ELEMENT</div>
+                    <div className={styles.elem}>ELEMENT</div>
+                    <div className={styles.elem}>ELEMENT</div>
+                    <div className={styles.elem}>ELEMENT</div>
+                    <div className={styles.elem}>ELEMENT</div>
+                    <div className={styles.elem}>ELEMENT</div>
+                </div>
+            </div>
+        </div>
+    )
 }
